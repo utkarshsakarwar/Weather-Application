33d7e2d686dcd62a8dcc8ad239e84e22a6e3a1d7
utkarshsakarwar <109698025+utkarshsakarwar@users.noreply.github.com>
2022-10-07 18:44:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let weather = {
-  apiKey: "55ac564a4fd444e8ad5a03ad63bee16d",
+  apikey: "55ac564a4fd444e8ad5a03ad63bee16d",
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
-        this.apiKey
+        this.apikey
     )
       .then((response) => {
         if (!response.ok) {
